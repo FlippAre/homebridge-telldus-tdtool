d0e46dfad3565cc451d26994bf6d8b8a7e47b9f8
Filip Persson <filip.persson@me.com>
2017-01-31 22:14:55 +0100

    refactor

--- src/lib/telldus-temperature.js
+++ src/lib/telldus-temperature.js
@@ -3,9 +3,6 @@
 const telldus                             = require('telldus')
 const TelldusAccessory                    = require('./telldus-accessory')
 const RateLimiter                         = require('limiter').RateLimiter
-const DailyMaxTemperatureCharacteristics  = require('./CustomCharacteristics/DailyMaxTemperature')
-
-
 
 /**
  * An Accessory convenience wrapper.
@@ -29,7 +26,25 @@ class TelldusTemperature extends TelldusAccessory {
     this.id = "sensor" + data.id
     this.service = new this.Service.TemperatureSensor(this.name)
     this.db = db
+    let Characteristic = homebridge.Characteristic
+
     
+    class DailyMaxTemperature extends Characteristic {
+    constructor() {
+        super()
+        Characteristic.call(this, 'Daily Max Temp', '00000011-0000-1000-8000-MAX6BB765291');
+        this.setProps({
+            format: Characteristic.Formats.FLOAT,
+            unit: Characteristic.Units.CELSIUS,
+            maxValue: 100,
+            minValue: -100,
+            minStep: 0.1,
+            perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY]
+        });
+        this.value = this.getDefaultValue();
+    }
+
+}
 
     this.service.addCharacteristic(this.Characteristic.CurrentRelativeHumidity)
     this.service.addCharacteristic(DailyMaxTemperatureCharacteristics)
